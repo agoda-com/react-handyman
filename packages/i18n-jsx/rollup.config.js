d66d357aa72189cb9e8922929d0e8b7066c7a6e3
matmalkowski <monkey3310@gmail.com>
2019-06-10 22:51:38 +0700

    Moving into mono repo 1

---
+++ packages/i18n-jsx/rollup.config.js
@@ -0,0 +1,34 @@
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import { terser } from "rollup-plugin-terser";
+
+import pkg from "./package.json";
+
+const extensions = [".ts", ".tsx"];
+
+const name = "i18n-jsx";
+
+export default {
+  input: "./src/index.ts",
+
+  // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
+  // https://rollupjs.org/guide/en#external-e-external
+  external: Object.keys(pkg.peerDependencies) || [],
+
+  plugins: [
+    // Allows node_modules resolution
+    resolve({ extensions }),
+
+    // Compile TypeScript/JavaScript files
+    babel({ extensions, include: ["src/**/*"], exclude: "node_modules/**" }),
+    terser()
+  ],
+
+  output: [
+    {
+      file: pkg.module,
+      format: "es",
+      name
+    }
+  ]
+};
