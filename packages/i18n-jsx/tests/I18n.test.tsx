051cdca27055bef3e2418f0aac99373a9087c2d9
matmalkowski <monkey3310@gmail.com>
2019-06-13 01:04:07 +0700

    Add more tests for I18n

--- packages/i18n-jsx/tests/I18n.test.tsx
+++ packages/i18n-jsx/tests/I18n.test.tsx
@@ -2,6 +2,8 @@ import 'jest'
 import * as React from 'react'
 import { render, cleanup, getNodeText } from '@testing-library/react'
 
+const consoleWarn = jest.spyOn(global.console, 'warn').mockImplementation(() => {})
+
 import I18n from '../src/I18n'
 import I18nProvider from '../src/I18nProvider'
 
@@ -12,7 +14,8 @@ const translationsMock = {
 
 describe('<I18n />', () => {
   afterEach(cleanup)
-  it('should render text based on a k prop', () => {
+
+  it('should render text based on a numeric k prop', () => {
     const { container } = render(
       <I18nProvider translations={translationsMock}>
         <span>
@@ -24,5 +27,33 @@ describe('<I18n />', () => {
     const innerText = getNodeText(container.querySelector('span'))
 
     expect(innerText).toEqual('number based key')
+  })
+
+  it('should render text based on a string k prop', () => {
+    const { container } = render(
+      <I18nProvider translations={translationsMock}>
+        <span>
+          <I18n k="example.key">Default value</I18n>
+        </span>
+      </I18nProvider>
+    )
+
+    const innerText = getNodeText(container.querySelector('span'))
+
+    expect(innerText).toEqual('string based key')
+  })
+
+  it('should render default fallback value when k key is not present in context', () => {
+    const { container } = render(
+      <I18nProvider translations={translationsMock}>
+        <span>
+          <I18n k="invalid.key.string">Default value</I18n>
+        </span>
+      </I18nProvider>
+    )
+
+    const innerText = getNodeText(container.querySelector('span'))
+
+    expect(innerText).toEqual('Default value')
   })
 })
