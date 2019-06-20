4dc988ff81638c3041096c8c951a9fab29ffea15
Maciej Małkowski <monkey3310@gmail.com>
2019-06-20 22:12:37 +0700

    Disable notFound logging when in test environment (#5)

--- packages/i18n-jsx/src/utils/handleNotFound.ts
+++ packages/i18n-jsx/src/utils/handleNotFound.ts
@@ -1,5 +1,5 @@
 const handleNotFound = (k: string | number, notFound: string) => {
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
     console.warn(`[i18n-jsx]: '${k}' key was not found in the translations object, falling back to notFound value.`)
   }
 
--- packages/i18n-jsx/tests/handleNotFound.test.ts
+++ packages/i18n-jsx/tests/handleNotFound.test.ts
@@ -2,7 +2,7 @@ import 'jest'
 
 const consoleWarn = jest.spyOn(global.console, 'warn').mockImplementation(() => {})
 
-import handleNotFound from '../src/utils/handleNotFound';
+import handleNotFound from '../src/utils/handleNotFound'
 
 describe('handleNotFound()', () => {
   const OLD_ENV = process.env
@@ -14,26 +14,34 @@ describe('handleNotFound()', () => {
   })
 
   afterEach(() => {
-    jest.clearAllMocks();
+    jest.clearAllMocks()
     process.env = OLD_ENV
   })
 
   it('should print console warning when node env is not production', () => {
-    process.env.NODE_ENV = 'dev';
+    process.env.NODE_ENV = 'dev'
 
-    handleNotFound(1, 'not found message');
-    
-    expect(consoleWarn).toHaveBeenCalledTimes(1);
-    expect(consoleWarn).toHaveBeenCalledWith('[i18n-jsx]: \'1\' key was not found in the translations object, falling back to notFound value.')
+    handleNotFound(1, 'not found message')
 
+    expect(consoleWarn).toHaveBeenCalledTimes(1)
+    expect(consoleWarn).toHaveBeenCalledWith(
+      "[i18n-jsx]: '1' key was not found in the translations object, falling back to notFound value."
+    )
   })
 
   it('should not print console warning when node env is production', () => {
-    process.env.NODE_ENV = 'production';
+    process.env.NODE_ENV = 'production'
 
-    handleNotFound(1, 'not found message');
-    
-    expect(consoleWarn).toHaveBeenCalledTimes(0);
+    handleNotFound(1, 'not found message')
 
+    expect(consoleWarn).toHaveBeenCalledTimes(0)
+  })
+
+  it('should not print console warning when node env is test', () => {
+    process.env.NODE_ENV = 'test'
+
+    handleNotFound(1, 'not found message')
+
+    expect(consoleWarn).toHaveBeenCalledTimes(0)
   })
 })
