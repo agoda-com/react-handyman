32978eb81fae58c1a50d87c4450f0e45ca581ed0
nshannon <Nicholas.Shannon@agoda.com>
2019-07-12 13:05:54 +0700

    Improved types for i18n HOC

--- packages/i18n-jsx/src/withI18n/withI18n.tsx
+++ packages/i18n-jsx/src/withI18n/withI18n.tsx
@@ -1,23 +1,24 @@
 import * as React from 'react'
 import useI18n from '../useI18n/useI18n'
 import { I18nSelector } from '../useI18n/selector'
+import { PropsWithChildren } from 'react'
 
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type Optionalize<T extends K, K> = Omit<T, keyof K>;
 
 const withI18n = <TProps extends TI18nProps, TI18nProps extends {}>(
-  Component: React.ComponentType<TProps & TI18nProps>,
+  Component: React.ComponentType<TProps>,
   mapI18nToProps: (i18n: I18nSelector) => TI18nProps
 ) => {
-  type OwnProps = Omit<TProps, keyof TI18nProps>
+  type OwnProps = Optionalize<TProps, TI18nProps>;
 
   const Wrapped: React.FC<OwnProps> = React.memo(props => {
     const i18n = useI18n()
-
     return <Component {...props as TProps} {...mapI18nToProps(i18n)} />
-  })
+  });
 
   Wrapped.displayName = `withI18n(${Component.displayName})`
-  return Wrapped
+  return Wrapped;
 }
 
 export default withI18n
