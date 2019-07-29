79b7e5604fde8ac40cd5ebe2d0060d3805deb0d5
matmalkowski <monkey3310@gmail.com>
2019-07-29 23:29:47 +0700

    test: adding more coverage for HOCs

---
+++ packages/ab-test-jsx/tests/withABTests.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import * as ABTestJsx from '../src/index'
+
+type Tests = {
+  test1: ABTestJsx.Variant
+  test2: ABTestJsx.Variant
+}
+
+const { withABTests, ABTestsProvider } = ABTestJsx as ABTestJsx.ABTestsModule<Tests>
+
+describe('withABTests', () => {
+  afterEach(() => {
+    cleanup()
+    jest.clearAllMocks()
+  })
+
+  it('should decorate component with context data', () => {
+    const abTests: Tests = {
+      test1: 'A',
+      test2: 'B',
+    }
+
+    type OwnProps = {
+      text: string
+    }
+
+    const UnderTest: React.FC<OwnProps & Tests> = ({ text, test1 }) => (
+      <>
+        <span>{text}</span>
+        {test1 === 'A' && <span>test1=A</span>}
+        {test1 === 'B' && <span>test1=B</span>}
+      </>
+    )
+
+    const UnderTestWithABTests = withABTests(UnderTest)
+
+    const { container } = render(
+      <ABTestsProvider abTests={abTests}>
+        <UnderTestWithABTests text="under test - " />
+      </ABTestsProvider>
+    )
+
+    expect(container.textContent).toEqual('under test - test1=A')
+  })
+})
