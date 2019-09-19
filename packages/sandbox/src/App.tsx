99963d21ed02cee50bf7b28460f06e50e12acdf3
Maciej Malkowski <maciej.malkowski@agoda.com>
2019-09-19 14:14:18 +0700

    fix: remove obsolete examples

--- packages/sandbox/src/App.tsx
+++ packages/sandbox/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import I18nExamples from './i18nExamples/I18nExamples';
-import FeatureToggleExamples from './featureToggleExamples/FeatureToggleExamples';
 
 const App: React.FC = () => (
   <div>
     <I18nExamples />
     <br />
     <br />
-    <FeatureToggleExamples />
   </div>
 );
 
