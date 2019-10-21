50c708ed08bbcb75c019aaf57055750fb3998d8e
Maciej Malkowski <maciej.malkowski@agoda.com>
2019-10-21 17:25:59 +0700

    fix(i18n-jsx): change default fallback variant to A
    Change default fallback variant for case when experiment cannot be allocated to A. Value can be
    modified by passing a defaultVariant setting to the context provider.
    
    BREAKING CHANGE: Default behavour of miss-allocation is different: A variant is used instead of Z

--- packages/ab-test-jsx/src/ABTestsContext/ABTestsContext.tsx
+++ packages/ab-test-jsx/src/ABTestsContext/ABTestsContext.tsx
@@ -2,4 +2,9 @@ import * as React from 'react';
 
 const ABTestsContext = React.createContext({});
 
+export type Settings = {
+  defaultVariant: string;
+}
+export const ABTestsSettings = React.createContext<Settings>({ defaultVariant: 'A' });
+
 export default ABTestsContext;
